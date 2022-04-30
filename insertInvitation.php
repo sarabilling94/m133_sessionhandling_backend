f338ccf77695b9476be06170eca9ca26f92b3c29
Sara <Sara@LAPTOP-SARA>
2022-04-30 22:58:46 +0200

    insert invitation, only if receiver hasn't received and isn't in group

---
+++ insertInvitation.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require 'connect.php';
+require 'accesscontrol.php';
+require 'userfunctions.php';
+require 'checkownerrights.php';
+
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata)){
+    $request = json_decode($postdata);
+     
+     
+    $receiverName = $request->receiverName;
+    $groupName = $request->groupName;
+    $senderId = $_SESSION["id_user"];
+
+    $allowed = checkCoOwnerRights($groupName, $db);
+    $userAlreadyInGroup = userIsInGroup($receiverName, $groupName, $db);
+    $alreadyInvited = receivedInvitation($receiverName, $groupName, $db);
+
+    if(($allowed == http_response_code(200)) && !$userAlreadyInGroup && !$alreadyInvited){
+        $receiverId = getUserId($receiverName, $db);
+        $groupId = getGroupId($groupName, $db);
+
+        if($receiverId && $groupId){
+            $sqlInsert = "INSERT INTO tbl_invitation (fk_sender,fk_receiver,fk_group) 
+            VALUES ($senderId,$receiverId,$groupId)";
+
+            $insertResult = mysqli_query ($db,$sqlInsert);
+            if($insertResult){
+                http_response_code(201);
+                echo "Invitation sent";
+            }
+        }
+        else{
+            http_response_code(404);
+            echo "User not found.";
+
+        } 
+    }
+    else{
+        http_response_code(401);
+        echo "Unauthorized, person is already in group or person has already received invitation.";
+    }
+}
+?> 
---
+++ userfunctions.php
@@ -0,0 +1,43 @@
+<?php
+require 'groupfunctions.php';
+
+function getUserId($name, $db){
+    $sql = "SELECT id_user FROM tbl_user WHERE name = '$name'";
+    $result = mysqli_query ($db,$sql);
+
+    if($result){
+        $data = mysqli_fetch_array ($result,MYSQLI_ASSOC);
+        return $data["id_user"];
+    }
+    return null;
+}
+
+function userIsInGroup($userName, $groupName, $db){
+    $userId = getUserId($userName, $db);
+    $groupId = getGroupId($groupName, $db);
+
+    $sql = "SELECT * FROM tbl_groupuser WHERE fk_user = $userId
+    AND fk_group = $groupId";
+
+    $result = mysqli_query($db, $sql);
+    if(mysqli_num_rows ($result) > 0){
+        return true;
+    }
+    return false;
+}
+
+function receivedInvitation($userName, $groupName, $db){
+    $userId = getUserId($userName, $db);
+    $groupId = getGroupId($groupName, $db);
+
+    $sql = "SELECT * FROM tbl_invitation WHERE fk_receiver = $userId
+    AND fk_group = $groupId";    
+
+    $result = mysqli_query ($db, $sql);
+    if(mysqli_num_rows ($result) > 0){
+        return true;
+    }
+    return false;
+}
+
+?>

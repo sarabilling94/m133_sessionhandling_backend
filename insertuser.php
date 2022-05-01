1e2c1505890cbed78cdd319dcef8f439b0673038
Sara <Sara@LAPTOP-SARA>
2022-05-01 20:18:42 +0200

    renamed insert

--- insertuser.php
+++ insertuser.php
@@ -1,5 +1,6 @@
 <?php
 require 'connect.php';
+require 'userfunctions.php';
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
@@ -13,13 +14,20 @@ if(isset($postdata) && !empty($postdata)){
     $password = mysqli_real_escape_string($db,$request->password);
     $hashedpassword = password_hash($password, PASSWORD_BCRYPT);
 
-    $sql = "INSERT INTO tbl_user (name,password) VALUES ('$name','$hashedpassword')";
-    if(mysqli_query($db,$sql)){
-        http_response_code(201);
+    $alreadyExists = userExists($name, $db);
+
+    if(!$alreadyExists){
+        $sql = "INSERT INTO tbl_user (name,password) VALUES ('$name','$hashedpassword')";
+        if(mysqli_query($db,$sql)){
+            http_response_code(201);
+        }
+        else{
+             http_response_code(422); 
+        }
     }
-    else{
-         http_response_code(422); 
-    }
-         
+     else{
+         http_response_code(409);
+         echo "User already exists.";
+     }    
 }
 ?> 
